63b2b0e6e6fa54bd51aadfe66408a739439a9dae
Menolo-Homobovanez <118155256+MenachemJacobs@users.noreply.github.com>
2024-12-18 13:06:35 -0500

    slack broadcaster

--- Components/GenericMessageObject.js
+++
@@ -1,7 +0,0 @@
-class GenericMessageObject {
-    constructor(sender, message_body, aside_ID) {
-        this.sender = sender;
-        this.message_body = message_body;
-        this.aside_ID = aside_ID;
-    }
-}
